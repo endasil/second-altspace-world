f239ccb48c9b1a83f8c1b5d518a1975e72da9067
Jonas <endasil@gmail.com>
2021-02-06 16:10:57 +0100

    updates

---
+++ Assets/find.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class find : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MonoBehaviour[] scriptsInScene = (MonoBehaviour[])FindObjectsOfType(typeof(MonoBehaviour));
+        Debug.Log("Objects fóund " + scriptsInScene.Length);
+        foreach (var scriptFile in scriptsInScene)
+        {
+            Debug.Log(scriptFile);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
